a740d356e0484e19b6a2496c8e1d471d7f15302c
Scott Gale <galejscott@gmail.com>
2019-05-16 10:16:53 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,13 +1,8 @@
 class Dog 
-  attr_accessor(:name, :breed)
   
-  def initialize(name)
+  
+  def initialize(name, breed = "Mutt")
     @name = name
     @breed = breed
   end
-  
-  def name
-    @name
-  end
-  
 end
